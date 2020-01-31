62ced3d31ade0f17e786541752f8f7405c9003ed
Glitch (inquisitive-spark-sgjwnrzvoq) <none>
2020-01-31 21:13:39 +0000

    🏈🏅 Checkpoint ./routes/index.js:5838347/1231

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
+'use strict'
+
 const fs = require('fs');
-const path = '../';
+const path = './.data/secure-connect.zip';
+const { Client } = require('cassandra-driver');
 
 var express = require('express');
 var router = express.Router();
@@ -14,10 +17,37 @@ router.get('/data', function (req, res) {
   
   const fs = require("fs");
   
-  exists = fs.existsSync(path);
-  if (fileExists)
+  var fileExists = fs.existsSync(path);
+  if (fileExists) {
+    theData = "The file exists.\n"
+    
+    process.env.USERNAME
+    
+    
+    
+  }
   
   res.send(theData);
 })
+
+
+
+// async function run() {
+//   const client = new Client({
+//     cloud: { secureConnectBundle: path },
+//     credentials: { username: 'username', password: 'password' }
+//   });
+
+//   await client.connect();
+
+//   // Execute a query
+//   const rs = await client.execute('SELECT * FROM system.local');
+//   console.log(`Hello from cluster: ${rs.first()['cluster_name']}`);
+
+//   await client.shutdown();
+// }
+
+// // Run the async function
+// run();
 
 module.exports = router;
