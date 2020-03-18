9d3bd2d0ca6ccece26f2eaba1ecd9248d045bdab
Glitch (glitchy-fluffy-barnacle) <none>
2020-03-18 00:07:40 +0000

    Import of riptano/better-botz-gui-at-glitch

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-# glitchy-fluffy-barnacle
+# better-botz-gui-at-glitch
 
 A sample Glitch website built with Node.js using Express and DataStax Apollo.
+
+To start the app:
+
+First add a `.data` directory and then copy your security bundle file to the path. An example of it being setup is shown in the app.js file in the following code snippet.
+
+``` javascript
+const path = './.data/secure-connect.zip';
+```
+
+Next, run `npm install` in the root directory of the project to retrieve all fo the project dependencies.
+
+Now run the server as shown:
+
+``` bash
+USERNAME=databaseUsername PASSWORD=databasePassword bash -c 'node bin/www'
+```
+
+Once running, check out the following paths for examples of data retrieval:
+
+http://localhost:3000/datareport >> this is a response via generated jade template w/ the data results.
+http://localhost:3000/data >> this is a direct data response of just the JSON response via API get call.
+http://localhost:3000/ >> verifies express.js is running with Jade templates!
--- routes/index.js
+++ routes/index.js
@@ -1,59 +1,39 @@
-'use strict'
+'use strict';
 
-const fs = require('fs');
 const path = './.data/secure-connect.zip';
 const { Client } = require('cassandra-driver');
+const client = new Client({
+  cloud: { secureConnectBundle: path },
+  credentials: { username: process.env.ASTRAUSER, password: process.env.ASTRAPASSWORD }
+});
 
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Better Botz Nous' });
+  res.render('index', { title: 'Better Botz' });
+});
+
+router.get('/datareport', function (req, res) {
+  getMoreData().then(function(data){
+    res.render('datareport', { data } );
+  }).catch(function(filteredData){
+    res.send(filteredData);
+  })
 });
 
 router.get('/data', function (req, res) {
-  var theData = 'Nope!';  
-  
-  var fileExists = fs.existsSync(path);
-  if (fileExists) {
-    theData = "The file exists.\n"
-    
-    theData += process.env.USERNAME
-    theData += process.env.PASSWORD  
-    
-    const client = new Client({
-      cloud: { secureConnectBundle: path },
-      credentials: { username: process.env.USERNAME, password: process.env.PASSWORD }
-    });
-    
-    client.connect();
+  getMoreData().then(function(data){
+    res.send(data);
+  }).catch(function(filteredData){
+    res.send(filteredData);
+  })
+});
 
-    // Execute a query
-    const rs = client.execute('SELECT * FROM system.local');
-    theData += rs.first()['cluster_name']
-    
-    client.shutdown();
-  
-    res.send(theData);
-  } else {
-    res.send("Dammit!");
-  }
-})
-
-async function getData() {
-  const client = new Client({
-    cloud: { secureConnectBundle: path },
-    credentials: { username: 'username', password: 'password' }
-  });
-
-  await client.connect();
-
-  // Execute a query
-  const rs = await client.execute('SELECT * FROM system.local');
-  console.log(`Hello from cluster: ${rs.first()['cluster_name']}`);
-
-  await client.shutdown();
+async function getMoreData(){
+  const result = await client.execute('SELECT customer_name, address, description, price, prod_id, prod_name, sell_price FROM betterbotz.orders');
+  return result.rows;
 }
 
 module.exports = router;
